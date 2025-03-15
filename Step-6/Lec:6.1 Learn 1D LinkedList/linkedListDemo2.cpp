6a7c2f1cbdfe381ef57e3901ee5ae8635d4ac5ca
MisariPanchal <116897139+MisariPanchal@users.noreply.github.com>
2025-03-15 14:26:03 +0000

    Print, Length ans Search in LinkedList

--- Step-6/Lec:6.1 Learn 1D LinkedList/linkedListDemo2.cpp
+++ Step-6/Lec:6.1 Learn 1D LinkedList/linkedListDemo2.cpp
@@ -1,36 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Node{
+struct Node{
     public:
         int data;
         Node* next;
-    
+
     public:
         Node(int data1, Node* next1){
             data = data1;
             next = next1;
         }
-    
-    //If you don't want to pass nullptr everytime
-    public:
-    Node(int data1){
-        data = data1;
-        next = nullptr;
-    }
+        Node(int data1){
+            data = data1;
+            next = nullptr;
+        }
 };
 
-Node* covertArraytoLL(vector<int> &arr){
-    Node* head = new Node(arr[0]);
-    Node* mover = head;
+Node* convertArrayToLL(vector<int> &arr){
+    Node* head = new Node(arr[0], nullptr);
+    Node* temp = head;
 
-    for(int i = 1; i < arr.size(); i++){
-        Node* temp = new Node(arr[i]);
-        mover->next = temp;
-        mover = temp;
+    for(int i = 1; i <= arr.size(); i++){
+        Node* newNode = new Node(arr[i], nullptr);
+        temp->next = newNode;
+        temp = newNode;
     }
+    
 
     return head;
+}
+
+int lengthOfLL(Node* head){
+    Node* current = head;
+    int length = 0;
+
+    while(current->next != nullptr){
+        length++;
+        current = current->next;
+    }
+
+    return length;
+}
+
+bool search(Node* head, int target){
+    Node* current = head;
+
+    while(current->next != nullptr){
+        if(current->data == target)return true;
+        current = current->next;
+    }
+
+    return false;
 }
 
 int main()
@@ -40,11 +61,31 @@ int main()
   freopen("../../input.txt", "r", stdin);
   freopen("../../output.txt", "w", stdout);
 #endif
-    vector<int> v1 = {14, 3, 2, 5, 6};
 
-    Node* head = covertArraytoLL(v1);
-    cout << "head->data : "<< head->data << endl;
-    
+    vector<int> v1 = {1, 2, 4, 6, 8};
 
-  return 0;
+    Node* head = convertArrayToLL(v1);
+    Node* current = head;
+
+    cout << "Printing LinkedList." << endl;
+    while(current->next != nullptr){
+        cout << "node->data : " << current->data;
+        cout << "  node->next : " << current->next << endl;
+
+        current = current->next;
+    }
+
+    cout << endl;
+
+    int len = lengthOfLL(head);
+
+    cout << "Length of a LinkedList : " << len << endl;
+
+    cout << endl;
+
+    cout << "Is element 22 present in Linkedlist : " << search(head, 22) << endl;
+    cout << "Is element 2 present in Linkedlist : " << search(head, 2) << endl;
+    cout << "Is element 27 present in Linkedlist : " << search(head, 27) << endl;
+    cout << "Is element 14 present in Linkedlist : " << search(head, 1) << endl;
+
 }
