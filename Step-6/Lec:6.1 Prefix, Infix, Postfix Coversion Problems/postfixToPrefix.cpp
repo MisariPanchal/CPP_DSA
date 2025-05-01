5048793a4bac11b5cfc9631dd369bd88cf475270
MisariPanchal <116897139+MisariPanchal@users.noreply.github.com>
2025-05-01 18:36:25 +0000

    Stack postfixToPrefix

---
+++ Step-6/Lec:6.1 Prefix, Infix, Postfix Coversion Problems/postfixToPrefix.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string postfixToPrefix(string s){
+    int n = s.size();
+
+    int i = 0;
+    stack<string> st;
+
+    while(i < n){
+        if((s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 'a' && s[i] <= 'z') || (s[i] >= '0' && s[i] <= '9')){
+            st.push(string(1, s[i]));
+        }
+        else{
+            string t1 = st.top();
+            st.pop();
+            string t2 = st.top();
+            st.pop();
+
+            st.push(s[i] + t2 + t1);
+        }
+
+        i++;
+    }
+
+    return st.top();
+}
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+  freopen("../../input.txt", "r", stdin);
+  freopen("../../output.txt", "w", stdout);
+#endif
+    
+    string s = "AB-DE+F*/";
+    string postToPre = postfixToPrefix(s);
+    cout << postToPre << endl;
+
+  return 0;
+}
